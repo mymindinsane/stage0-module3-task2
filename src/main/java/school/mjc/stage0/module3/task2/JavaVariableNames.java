87235dc044df219a4fa3babbca7aa5561f9e2d0b
mymindinsane <arturguletskiy21@gmail.com>
2024-02-27 17:08:20 +0300

    some fixes

--- src/main/java/school/mjc/stage0/module3/task2/JavaVariableNames.java
+++ src/main/java/school/mjc/stage0/module3/task2/JavaVariableNames.java
@@ -3,7 +3,7 @@ package school.mjc.stage0.module3.task2;
 public class JavaVariableNames {
     public static void main(String[] args) {
         int tomorrow = 0;
-        int $money = 10;
+        int money = 10;
         int hundred = 100;
         int back = 1000;
         int underscore = 10000;
